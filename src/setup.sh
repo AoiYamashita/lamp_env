77d8623372241adfedebf736862f1e1df13522db
Yamashita <c3260@st.cs.kumamoto-u.ac.jp>
2024-02-04 00:23:54 +0900

    write install apps

---
+++ src/setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+#------------- add GPGkey
+apt-get install curl gnupg lsb-release -y
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+#------------- add source list in repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+#------------- install apps
+apt update
+apt-get install apache2 apache2-dev
+apt install mysql-server mysql-client
+apt install -y php
